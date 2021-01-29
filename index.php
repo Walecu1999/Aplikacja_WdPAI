7674213debb315f648d70a188adb7579b14d631b
walecu1999 <maciekht@interia.pl>
2021-01-29 14:41:16 +0100

    try

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('index', 'DefaultController');
+Router::get('main', 'DefaultController');
+
+Router::run($path);
