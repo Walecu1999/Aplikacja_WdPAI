302b0526161223728cb3b133c1a35b79cd3fba13
walecu1999 <maciekht@interia.pl>
2021-01-29 14:30:24 +0100

    database

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+const USERNAME = 'qaxnlteyhyptvj';
+const PASSWORD = '0445ab13306fec66f2195ffaa6464ef85db495233d1c81ed32ddad6d739e1920';
+const HOST = 'ec2-18-203-62-227.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'devl5boo2firdt';
